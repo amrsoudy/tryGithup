9f731a4c1af323ad67ae57b396112cfb4386b14d
amrsoudy <32444508+amrsoudy@users.noreply.github.com>
2018-05-23 08:59:03 -0400

    class updated

--- src/persistance/Student.java
+++ src/persistance/Student.java
@@ -5,10 +5,51 @@
  */
 package persistance;
 
+import java.util.Date;
+
 /**
  *
  * @author 1795162
  */
 public class Student extends Person{
+    
+    
+    String student_id;
+    String lastname;
+    String firstName;
+    Date dateBirth;
+
+    public Student(String n, String p, Date d) {
+        this.lastname = n;
+        this.firstName = p;
+        this.dateBirth = d;
+        
+        this.student_id =createsStudent(n,p,d);
+    }
+
+    private String createsStudent(String lname, String fname, Date date) {
+        String end = concate(date);
+        return lname.substring(0,3)+fname.substring(0)+end;
+        
+        
+    }
+
+    private String concate(Date date) {
+        return date.getYear()+ "-"+date.getMonth();
+    }
+
+    public String getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(String student_id) {
+        this.student_id = student_id;
+    }
+    
+    public void isSuperViseBy(Professor professor){
+        professor.students.add(this);
+    
+    
+    }
     
 }
