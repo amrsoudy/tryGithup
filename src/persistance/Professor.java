9c0a47f2b45d7607c75eb69941aa1328fa7ff543
amrsoudy <32444508+amrsoudy@users.noreply.github.com>
2018-05-23 09:43:49 -0400

    refactor introduce methode  for professor

--- src/persistance/Professor.java
+++ src/persistance/Professor.java
@@ -19,18 +19,22 @@ public class Professor extends Person {
     String photour1;
     
     public Professor(String lastname,String firstname){
-        String  employeeId =lastname.toUpperCase().trim().substring(1,3)+firstname.toUpperCase().trim().substring(1,3);
+        String  employeeId =FormatEmployeeId(lastname, firstname);
         
         System.out.println("new prof " + employeeId);
 
 
 
 }
+
+    private String FormatEmployeeId(String lastname1, String firstname) {
+        return lastname1.toUpperCase().trim().substring(1, 3) + firstname.toUpperCase().trim().substring(1,3);
+    }
     
-    public void setPhotour(String photoName){
+    public void setPhotour(String photoName, String photoBath){
     
     
-    this.photour1 = "/photo/professors/mad/"+photoName;
+    this.photour1 = photoBath+photoName;
     }
 
     public void isSuperViseBy(Student student) {
